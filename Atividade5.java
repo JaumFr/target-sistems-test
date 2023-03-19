2a98471e8b2f572329e908c0d5912f9a38c39177
Jaum <joaovitor_rios@hotmail.com>
2023-03-18 21:47:25 -0300

    fix: close scanner and add readme

--- Atividade5.java
+++ Atividade5.java
@@ -4,16 +4,18 @@ public class Atividade5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-            System.out.print("Digite uma string: ");
-            String original = input.nextLine();
-            String invertida = "";
-        
-                for(int i = original.length()-1; i >= 0; i--) {
-                    invertida += original.charAt(i);
-                }
-        
-            System.out.println("String original: " + original);
-            System.out.println("String invertida: " + invertida);
+        System.out.print("Digite uma string: ");
+        String original = input.nextLine();
+        String invertida = "";
+    
+            for(int i = original.length()-1; i >= 0; i--) {
+                invertida += original.charAt(i);
+            }
+    
+        System.out.println("String original: " + original);
+        System.out.println("String invertida: " + invertida);
+
+        input.close();
     }
 }
 
